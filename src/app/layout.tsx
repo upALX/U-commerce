248341033f210f72f23bb05b29b3ad76263c0d01
José Alexandre <alexandreaddres@gmail.com>
2024-10-02 14:32:33 -0300

    adding pre configs to the app

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-inter",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "U-commerce",
+  description: "E-commerce simple",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html  className={`${geistSans.variable} antialiased`} lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
